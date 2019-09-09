6f7449f4a168a6527eec88ae5906e272c1ddfb41
pengchujin <2093959501@qq.com>
2019-09-09 16:18:11 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      Test
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {},
+
+  created () {}
+}
+</script>
+
+<style scoped>
+
+</style>
