a073f366353ab880515e9ca15d897a9e4055e922
pengchujin <2093959501@qq.com>
2019-09-10 16:26:38 +0800

    。

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-var Fly=require("flyio/dist/npm/wx") 
-var fly=new Fly
+import Fly from 'flyio/dist/npm/wx'
+const fly = new Fly()
 
-Vue.prototype.$http=fly 
 Vue.config.productionTip = false
 App.mpType = 'app'
-
+Vue.prototype.$http = fly
 const app = new Vue(App)
 app.$mount()
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,21 +1,44 @@
 <template>
   <div>
     <div>
-      Test
+      <ul id="example-1">
+      <li v-for="item in test" :key="item" >
+        {{ item.heading.title }}
+        {{ item.heading.subtitle }}
+      </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-
+// this.$http.get("https://www.shazam.com/charts/top-200/united-states").then((d)=>{
+//       //输出请求数据
+//       console.log(d.data)
+//       //输出响应头
+//       console.log(d.header)
+//     }).catch(err=>{
+//       console.log(err.status,err.message)
+//     })
 export default {
   data () {
     return {
+      test: []
     }
   },
+  mounted () {
+    console.log('Mouted')
+    this.$http.get('https://sebs.qust.me/shazam/v2/en-US/US/web/-/tracks/country-chart-US?pageSize=200&startFrom=0').then((d) => {
+      this.test = d.data.chart
+      console.log(d.data)
+      console.log(d.header)
+    }).catch(err => {
+      console.log(err.status, err.message)
+    })
+  },
   methods: {},
-
-  created () {}
+  created () {
+  }
 }
 </script>
 
